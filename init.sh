9f0cdc8213cc6087d126b20383f51babf83c07bb
Davi Silva <davi.cunha.silva.dev@gmail.com>
2023-05-22 15:08:35 +0200

    chore: update init script

--- init.sh
+++ init.sh
@@ -2,4 +2,16 @@
 
 git submodule update --recursive --init
 
+cd api-chat-cryptic-activist && git branch main
+
+cd api-cryptocurrency-cryptic-activist && git branch main
+
+cd api-fiat-cryptic-activist && git branch main
+
+cd api-offer-cryptic-activist && git branch main
+
+cd api-user-cryptic-activist && git branch main
+
+cd cryptic-activist-catalog && git branch main
+
 docker compose up -d
