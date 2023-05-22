74ecbfdf3e915b7d4d3ee7c74837392dd7bf6b5d
Davi Silva <davi.cunha.silva.dev@gmail.com>
2023-05-22 15:18:03 +0200

    .

--- init.sh
+++ init.sh
@@ -2,16 +2,16 @@
 
 git submodule update --recursive --init
 
-cd api-chat-cryptic-activist && git branch main && cd ..
+cd api-chat-cryptic-activist && git checkout main && cd ..
 
-cd api-cryptocurrency-cryptic-activist && git branch main && cd ..
+cd api-cryptocurrency-cryptic-activist && git checkout main && cd ..
 
-cd api-fiat-cryptic-activist && git branch main && cd ..
+cd api-fiat-cryptic-activist && git checkout main && cd ..
 
-cd api-offer-cryptic-activist && git branch main && cd ..
+cd api-offer-cryptic-activist && git checkout main && cd ..
 
-cd api-user-cryptic-activist && git branch main && cd ..
+cd api-user-cryptic-activist && git checkout main && cd ..
 
-cd cryptic-activist-catalog && git branch main && cd ..
+cd cryptic-activist-catalog && git checkout main && cd ..
 
 docker compose up -d
