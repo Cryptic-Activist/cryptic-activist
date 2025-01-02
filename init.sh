2c041c14986d35b1ec0c22dba66c59c38bb9ce40
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-01-02 20:04:41 +0100

    chore: updates stack

--- init.sh
+++ init.sh
@@ -2,23 +2,23 @@
 
 git submodule update --recursive --init
 
-cd api-chat-cryptic-activist && git checkout main && yarn && cd ..
+cd api-chat-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-cryptocurrency-cryptic-activist && git checkout main && yarn && cd ..
+cd api-cryptocurrency-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-fiat-cryptic-activist && git checkout main && yarn && cd ..
+cd api-fiat-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-offer-cryptic-activist && git checkout main && yarn && cd ..
+cd api-offer-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-trade-cryptic-activist && git checkout main && yarn && cd ..
+cd api-trade-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-user-cryptic-activist && git checkout main && yarn && cd ..
+cd api-user-cryptic-activist && git checkout main && npm install && cd ..
 
-cd cryptic-activist-catalog && git checkout main && yarn && cd ..
+cd cryptic-activist-catalog && git checkout main && npm install && cd ..
 
-cd admin-cryptic-activist-catalog && git checkout main && yarn && cd ..
+cd admin-cryptic-activist-catalog && git checkout main && npm install && cd ..
 
-cd new-cryptic-activist-catalog && git checkout main && yarn && cd ..
+cd new-cryptic-activist-catalog && git checkout main && npm install && cd ..
 
 docker compose up -d
 
