1f2d292647e0f6c289b7889f361bf780282bef85
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-02-12 16:07:42 +0100

    chore: update inish.sh

--- init.sh
+++ init.sh
@@ -2,24 +2,24 @@
 
 git submodule update --recursive --init
 
-cd api-chat-cryptic-activist && git checkout main && npm install && cd ..
+# cd api-chat-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-cryptocurrency-cryptic-activist && git checkout main && npm install && cd ..
+# cd api-cryptocurrency-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-fiat-cryptic-activist && git checkout main && npm install && cd ..
+# cd api-fiat-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-offer-cryptic-activist && git checkout main && npm install && cd ..
+# cd api-offer-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-trade-cryptic-activist && git checkout main && npm install && cd ..
+# cd api-trade-cryptic-activist && git checkout main && npm install && cd ..
 
-cd api-user-cryptic-activist && git checkout main && npm install && cd ..
+# cd api-user-cryptic-activist && git checkout main && npm install && cd ..
 
-cd cryptic-activist-catalog && git checkout main && npm install && cd ..
+# cd cryptic-activist-catalog && git checkout main && npm install && cd ..
 
-cd admin-cryptic-activist-catalog && git checkout main && npm install && cd ..
+# cd admin-cryptic-activist-catalog && git checkout main && npm install && cd ..
 
-cd new-cryptic-activist-catalog && git checkout main && npm install && cd ..
+# cd new-cryptic-activist-catalog && git checkout main && npm install && cd ..
 
 docker compose up -d
 
-docker container exec postgresql psql -U postgres -d db-ca < dump.sql
+# docker container exec postgresql psql -U postgres -d db-ca < dump.sql
