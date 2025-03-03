d296e614cc06c8be97976bc7918fb880753d92c3
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-03-03 15:05:12 +0100

    chore: cleans package.json from used packages

--- libraries/base-ca/src/utils/map/models/index.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from 'sequelize';
-
-export const mapAssociations = (model: Model) =>
-  model.get({ plain: true });
-
-export const mapArrayAssociations = (models: Model[]) =>
-  models.map((model) => model.get({ plain: true }));
