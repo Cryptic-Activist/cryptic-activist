7a2231644b480d513ca3e77bae21f867a7389c75
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-03-31 17:57:02 +0200

    chore: disables dts files on local packages

--- libraries/base-ca/tsup.config.ts
+++ libraries/base-ca/tsup.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'tsup';
 
+const IS_DEV = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
   entry: ['src'],
   splitting: false,
   sourcemap: true,
   clean: true,
-  dts: true,
+  dts: IS_DEV,
 });
