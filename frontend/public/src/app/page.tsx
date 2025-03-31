fd3f6c52cab295b7010c7e87b24ad2d03b550c21
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-03-31 18:27:13 +0200

    feat: add Hello World! header to the homepage

--- frontend/public/src/app/page.tsx
+++ frontend/public/src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <h1>Hello World!</h1>
       <OfferList />
     </div>
   );
