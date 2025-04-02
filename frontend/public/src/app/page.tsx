f0a56b2869d6ad925b585cc9a8a820797f0f0e1d
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 08:24:56 +0200

    fix: add love message to homepage

--- frontend/public/src/app/page.tsx
+++ frontend/public/src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <h1>Deployed via Github Actions</h1>
       <h2>April 2025</h2>
+      <h3>I love you</h3>
       <OfferList />
     </div>
   );
