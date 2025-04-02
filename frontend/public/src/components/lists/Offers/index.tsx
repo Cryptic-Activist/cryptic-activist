cd6026dd54bbed78515c30f4686ef44d9dc586a2
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 15:26:57 +0200

    fix: reorder environment variable exports for consistency and add NODE_ENV logging

--- frontend/public/src/components/lists/Offers/index.tsx
+++ frontend/public/src/components/lists/Offers/index.tsx
@@ -3,7 +3,9 @@
 import { FC } from 'react';
 import Header from './Header';
 import List from './List';
+import { NODE_ENV } from '@/constants';
 import { OffersProps } from './types';
+import { constrainedMemory } from 'process';
 import styles from './index.module.scss';
 import { useApp } from '@/hooks';
 
@@ -11,6 +13,8 @@ const Offers: FC<OffersProps> = ({ height }) => {
   const {
     app: { currentPrice, type },
   } = useApp();
+
+  console.log({ NODE_ENV });
   return (
     <div className={styles.container}>
       <Header />
