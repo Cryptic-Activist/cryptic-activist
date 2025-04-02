a6c21f949584f30d3540571bec6d688a15cc6e55
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 11:40:29 +0200

    fix: remove unnecessary deployment messages and adjust command timeout in GitHub Actions workflow

--- frontend/public/src/app/page.tsx
+++ frontend/public/src/app/page.tsx
@@ -4,9 +4,6 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Deployed via Github Actions</h1>
-      <h2>April 2025</h2>
-      <h3>I love you</h3>
       <OfferList />
     </div>
   );
