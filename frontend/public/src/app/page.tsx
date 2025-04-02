2854772577cb2fdf48b8e4743091dcb3586827b5
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 12:10:20 +0200

    fix: remove love message from the homepage

--- frontend/public/src/app/page.tsx
+++ frontend/public/src/app/page.tsx
@@ -4,7 +4,6 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>{`I <3 u`}</h1>
       <OfferList />
     </div>
   );
