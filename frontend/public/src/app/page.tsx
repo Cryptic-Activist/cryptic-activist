92509cdcb4d71be280152c3297a57874507d98f9
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 11:56:08 +0200

    feat: add a love message to the homepage

--- frontend/public/src/app/page.tsx
+++ frontend/public/src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <h1>{`I <3 u`}</h1>
       <OfferList />
     </div>
   );
