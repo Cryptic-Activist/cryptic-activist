2f6e1ec0d89d59924823c4a8ce06ec3134c9fe07
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 18:36:16 +0200

    fix: enhance environment variable logging and add public variable definitions in production Dockerfile

--- frontend/public/src/constants/envs/index.ts
+++ frontend/public/src/constants/envs/index.ts
@@ -3,5 +3,3 @@ export const IS_DEVELOPMENT = NODE_ENV === 'development';
 
 export const APP_NAME = process.env.APP_NAME as string;
 export const BACKEND = process.env.BACKEND as string;
-
-console.log({ BACKENDED: process.env.BACKEND });
