eb85ddfda1bdc709279ef8825bae0c50ee79bb84
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 19:16:22 +0200

    fix: update environment variable syntax and references in Dockerfiles and config files

--- frontend/public/next.config.ts
+++ frontend/public/next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next';
 
-export const NODE_ENV = process.env.NODE_ENV as string;
-export const IS_DEVELOPMENT = NODE_ENV === 'development';
-
-console.log({ ENV: process.env });
-
 const nextConfig: NextConfig = {
   output: 'standalone',
-  env: {
-    APP_NAME: IS_DEVELOPMENT
-      ? process.env.APP_NAME
-      : process.env.NEXT_PUBLIC_APP_NAME,
-    BACKEND: IS_DEVELOPMENT
-      ? process.env.BACKEND
-      : process.env.NEXT_PUBLIC_BACKEND,
-  },
   images: {
     remotePatterns: [
       {
--- frontend/public/src/constants/envs/index.ts
+++ frontend/public/src/constants/envs/index.ts
@@ -2,4 +2,4 @@ export const NODE_ENV = process.env.NODE_ENV as string;
 export const IS_DEVELOPMENT = NODE_ENV === 'development';
 
 export const APP_NAME = process.env.APP_NAME as string;
-export const BACKEND = process.env.BACKEND as string;
+export const BACKEND = process.env.NEXT_PUBLIC_BACKEND as string;
