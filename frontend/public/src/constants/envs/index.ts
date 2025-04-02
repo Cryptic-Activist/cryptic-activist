3ee2d07b75c1eeb707d94aad14c096421b84c421
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-02 18:18:00 +0200

    fix: update environment variable handling for development and production contexts

--- frontend/public/src/constants/envs/index.ts
+++ frontend/public/src/constants/envs/index.ts
@@ -1,11 +1,7 @@
 export const NODE_ENV = process.env.NODE_ENV as string;
 export const IS_DEVELOPMENT = NODE_ENV === 'development';
 
-export const APP_NAME = IS_DEVELOPMENT
-  ? (process.env.APP_NAME as string)
-  : (process.env.NEXT_PUBLIC_FRONTEND_PUBLIC as string);
-export const BACKEND = IS_DEVELOPMENT
-  ? (process.env.BACKEND as string)
-  : (process.env.NEXT_PUBLIC_BACKEND as string);
+export const APP_NAME = process.env.APP_NAME as string;
+export const BACKEND = process.env.BACKEND as string;
 
 console.log({ BACKENDED: process.env.BACKEND });
