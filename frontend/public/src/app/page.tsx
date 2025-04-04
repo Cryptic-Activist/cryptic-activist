c0bca359dc5bcf77332bb818692631f59b7c95ab
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-04 17:17:41 +0200

    refactor: remove test heading from Home component

--- frontend/public/src/app/page.tsx
+++ frontend/public/src/app/page.tsx
@@ -4,7 +4,6 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Test</h1>
       <OfferList />
     </div>
   );
