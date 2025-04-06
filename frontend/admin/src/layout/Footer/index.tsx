85100aaaa0f07cf0191a1d67f7a2925c60084f26
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-06 16:03:57 +0200

    chore: test

--- frontend/admin/src/layout/Footer/index.tsx
+++ frontend/admin/src/layout/Footer/index.tsx
@@ -1,14 +1,14 @@
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <p className={styles.statement}>
-        &copy;{" "}
-        {`${new Date().getFullYear()} Cryptic Activist Catalog - All rights reserved`}
-      </p>
-    </footer>
-  );
+	return (
+		<footer>
+			<p>
+				&copy;{' '}
+				{`${new Date().getFullYear()} Cryptic Activist Catalog - All rights reserved`}
+			</p>
+		</footer>
+	);
 };
 
 export default Footer;
