d10a529ffe1e8155f8cc1fadc73ec6e51e2cc435
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-06 16:06:11 +0200

    chore: test

--- frontend/admin/src/layout/NavigationBar/index.tsx
+++ frontend/admin/src/layout/NavigationBar/index.tsx
@@ -1,51 +1,48 @@
-import { useAdmin } from "@/hooks";
-import { openModal } from "@/stores/navigationBar";
-import { isLoggedIn } from "@/utils/checks/admin";
-import Link from "next/link";
-
-import styles from "./styles.module.scss";
+import Link from 'next/link';
+import { isLoggedIn } from '@/utils/checks/admin';
+import { openModal } from '@/stores/navigationBar';
+import styles from './styles.module.scss';
+import { useAdmin } from '@/hooks';
 
 const NavigationBar = () => {
-  const { admin } = useAdmin();
+	const { admin } = useAdmin();
 
-  const toggleLogin = () => {
-    openModal({ modal: "login" });
-  };
+	// const toggleLogin = () => {
+	//   openModal({ modal: "login" });
+	// };
 
-  return (
-    <nav className={styles.nav}>
-      <div className={styles.container}>
-        <Link href="/" className={styles.brand}>
-          <div className={styles.brandPrimary}>
-            <h1>Cryptic</h1>
-            <h2>Activist</h2>
-          </div>
-          <div className={styles.sep}></div>
-          <div className={styles.brandSecondary}>
-            <h3>Admin</h3>
-          </div>
-        </Link>
-        <ul className={styles.menu}>
-          <li>
-            <Link href="/" className={styles.menuItem}>
-              Home
-            </Link>
-          </li>
-          <li>
-            {isLoggedIn(admin.data) ? (
-              <button className={styles.userButton}>
-                {admin.data?.firstName}
-              </button>
-            ) : (
-              <button className={styles.userButton} onClick={toggleLogin}>
-                Login
-              </button>
-            )}
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className={styles.nav}>
+			<div className={styles.container}>
+				<Link href="/" className={styles.brand}>
+					<div className={styles.brandPrimary}>
+						<h1>Cryptic</h1>
+						<h2>Activist</h2>
+					</div>
+					<div className={styles.sep}></div>
+					<div className={styles.brandSecondary}>
+						<h3>Admin</h3>
+					</div>
+				</Link>
+				<ul className={styles.menu}>
+					<li>
+						<Link href="/" className={styles.menuItem}>
+							Home
+						</Link>
+					</li>
+					<li>
+						{isLoggedIn(admin.data) ? (
+							<button className={styles.userButton}>
+								{admin.data?.firstName}
+							</button>
+						) : (
+							<button className={styles.userButton}>Login</button>
+						)}
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default NavigationBar;
