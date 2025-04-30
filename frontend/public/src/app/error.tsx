0c90ec3818774349210c4f6072a27a4e058593e8
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-04-30 10:15:15 +0200

    feat(error): add error component with styling

---
+++ frontend/public/src/app/error.tsx
@@ -0,0 +1,9 @@
+import styles from './error.module.scss';
+
+export default function Error() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Something went wrong!</h1>
+    </div>
+  );
+}
