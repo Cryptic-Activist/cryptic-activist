a9dda7bdfe860674091827fa7f655a09fce97511
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-05-23 18:12:39 +0200

    feat: enhance nginx configuration to block access to common sensitive file paths; add sitemap.xml and robots.txt for improved SEO

---
+++ frontend/public/src/app/robots.ts
@@ -0,0 +1,13 @@
+import { APP_URL } from '@/constants';
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: `${APP_URL}/sitemap.xml`,
+  };
+}
